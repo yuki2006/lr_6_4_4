610f8782a5dbb44f241266413fe459d97c66ce4a
yuki2006 <yagfair@gmail.com>
2023-06-22 16:14:41 +0900

    bug

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { ColorDataTyp } from "./ColorProvider";
 import { useColors } from "../hooks/useColors";
@@ -7,16 +7,21 @@ import { StarRating } from "./StarRating";
 export function Color(props: ColorDataTyp) {
   const { id, title, color, rating } = props
   const { rateColor, removeColor } = useColors();
+  const [count, setCount] = useState(0);
+
   return (
     <section>
-      <h1>{title}</h1>
+      <h1>{title} -- click count {count} </h1>
       <button onClick={() => removeColor(id)}>
         <FaTrash />
       </button>
       <div style={{ height: 50, backgroundColor: color }} />
       <StarRating
         selectedStars={rating}
-        onRate={rating => rateColor(id, rating)}
+        onRate={rating => {
+          rateColor(id, rating);
+          setCount(count+1);
+        }}
       />
     </section>
   );
